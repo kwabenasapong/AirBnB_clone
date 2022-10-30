9955d567838f853fc3f9e336dc434586cbbaa9dd
kwabenasapong <kwabenasapong@gmail.com>
2022-10-30 17:15:46 +0000

    pycodestyle check

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,14 +33,14 @@ class FileStorage():
         from models.review import Review
         from models.state import State
         from models.user import User
- 
+
         classes = {
             "BaseModel": BaseModel,
-            "Amenity" : Amenity,
-            "City" : City,
-            "Place" : Place,
-            "Review" : Review,
-            "State" : State,
+            "Amenity": Amenity,
+            "City": City,
+            "Place": Place,
+            "Review": Review,
+            "State": State,
             "User": User
         }
 
