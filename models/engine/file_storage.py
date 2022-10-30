0264533a32ad55dac379e19fa5bdb6f246a1998b
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-10-30 17:12:43 +0000

    Update done for task 9 and 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,10 +27,20 @@ class FileStorage():
     def reload(self):
         """Deserialize the JSON in __file_path if it exists"""
         from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
         from models.user import User
-
+ 
         classes = {
             "BaseModel": BaseModel,
+            "Amenity" : Amenity,
+            "City" : City,
+            "Place" : Place,
+            "Review" : Review,
+            "State" : State,
             "User": User
         }
 
