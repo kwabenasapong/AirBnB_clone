bd554957bc7ca24fd70c4a50fb45e3a8cf1df327
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-10-30 17:02:05 +0000

    update place

--- models/place.py
+++ models/place.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
-'''User that inherits from BaseModel'''
+'''Place that inherits from BaseModel'''
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    '''Base class for user models'''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class Place(BaseModel):
+    '''Base class for place models'''
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
 
     def __init__(self, *args, **kwargs):
         '''Inititalize class'''
