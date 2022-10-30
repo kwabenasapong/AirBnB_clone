73d87a7cde76a1072c17e5cd6a75005f3a5bb08e
kwabenasapong <kwabenasapong@gmail.com>
2022-10-30 21:36:25 +0000

    rm conflicting files

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-
-
-'''instance of FileStorage'''
-storage = FileStorage()
-
-'''reload() method called on storage instance'''
-storage.reload()
